06f1309737fb24e14f24e824b8def0010bc2539d
Antonio Alvarez <antonio.alvarez15@stjohns.edu>
2019-12-04 16:43:14 -0500

    Third Commit
    updated models.py

--- CUS1166_Project_Template-master/app/main/personal_main/models.py
+++ CUS1166_Project_Template-master/app/main/personal_main/models.py
@@ -3,14 +3,21 @@
 from app import db
 
 
-class Appointment(db.model):
-    __tablename__ = "Appointments"
-    id = db.Column(db.Integer, primary_key=True)
 
-    app_title = db.Column(db.String, nullable=False)
-    app_date = db.Column(db.String, nullable=False)
+
+class Customer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), index=True)
+    address = db.Column(db.String(128), index=True)
+
+
+class Appointment(db.model):
+    id = db.Column(db.Integer, primary_key=True)
+    app_title = db.Column(db.String(128), nullable=False)
+    app_date = db.Column(db.String(128), nullable=False)
     start_time = db.Column(CharField(max_length=5), nullable=False)
-    duration = db.Column(db.String, nullable=False)
-    app_location = db.Column(db.String, nullable=False)
-    customer_name = db.Column(db.String, nullable=False)
-    app_notes = db.Column(db.String, nullable=True)
+    duration = db.Column(db.String(128), nullable=False)
+    app_location = db.Column(db.String(128), nullable=False)
+    customer_id = db.Column(db.Integer, db.ForeignKey('Customer.id') nullable=False)
+    app_notes = db.Column(db.String(128), nullable=True)
+    app_status = db.Column(db.String(128), nullable=True)
---
+++ CUS1166_Project_Template-master/app/main/personal_main/routes.py
@@ -0,0 +1,35 @@
+from flask import render_template,  redirect, url_for
+from app.main import bp
+from app import db
+from app.main.forms import TaskForm
+from app.models import Appointments
+
+
+@bp.route('/alvarez')
+def index():
+    return render_template("home.html")
+
+
+@bp.route('/appointments', methods=['GET','POST'])
+def todolist():
+    form = TaskForm()
+
+    if form.validate_on_submit():
+        # Get the data from the form, and add it to the database.
+        new_appointment = Appointment()
+        new_appointment.app_title =  form.app_title.data
+        new_appointment.app_date =  form.app_date.data
+        new_appointment.start_time = form.start_time.data
+        new_appointment.app_duration = form.app_duration.data
+        new_appointment.app_notes = form.app_notes.data
+        new_appointment.app_status = form.app_status.data
+
+        db.session.add(new_appointment)
+        db.session.commit()
+
+        # Redirect to this handler - but without form submitted - gets a clear form.
+        return redirect(url_for('')) # Add route for the appointment list
+
+    todo_list = db.session.query(Appointment).all()
+
+    return render_template("main/todolist.html",todo_list = todo_list,form= form)  # edit for appointments list route
